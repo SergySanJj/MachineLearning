1e4ab1afae98a7018bb30901e87e7539a96df8df
Sergei Yarema <isara.isara8@gmail.com>
2018-03-14 21:13:04 +0300

    little changes

--- NeuralNetwork/NeuralNetwork/Player.cpp
+++ NeuralNetwork/NeuralNetwork/Player.cpp
@@ -10,6 +10,12 @@ Player::Player(int n, int m, int playerID)
 	std::uniform_real_distribution<int> distx(0, std::nextafter(n-2, INT_MAX));
 	std::uniform_real_distribution<int> disty(0, std::nextafter(m-2, INT_MAX));
 	*/
+	srand(time(0));
+	int rndx = rand() + n;
+	int rndy = rand() + m;
+
+	this->pos_x = rndx;
+	this->pos_y = rndy;
 
 	this->neuro = new NeuralNetwork("payer_" + to_string(this->playerID));
 }
